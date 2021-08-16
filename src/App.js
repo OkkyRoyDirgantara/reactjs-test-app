5e7ebd96fbdc040f55188a717c6ebcc7261be6cf
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-16 19:49:01 +0700

    Add Component from ComponentReact.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {JavaScript,JSX} from "./Components/IntroductionJSX";
+import {FoodsMenu,DrinksMenu} from "./Components/ComponentReact";
 
 function App() {
   return (
     <div>
       <JavaScript />
         <JSX/>
-
+        <FoodsMenu/>
+      <DrinksMenu/>
     </div>
   );
 }
