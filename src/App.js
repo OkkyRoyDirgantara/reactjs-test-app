a719e5634f1eedfb19de95acbe4166fc746a11c0
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-16 20:38:41 +0700

    State ReactJS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import {JavaScript,JSX} from "./Components/IntroductionJSX";
 import {FoodsMenu,DrinksMenu} from "./Components/ComponentReact";
 
-function App() {
-  return (
-    <div>
-      <JavaScript />
-        <JSX/>
-        <FoodsMenu/>
-      <DrinksMenu/>
-    </div>
-  );
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            nameResto: "Dirga",
+        }
+    }
+    render() {
+        return (
+            <div>
+                <JavaScript />
+                <JSX/>
+                <div className={"App"}>
+                    <h1>Cafe and Resto {this.state.nameResto}</h1>
+                    <h2>Foods Menu</h2>
+                    <FoodsMenu nameMenu={"Chicken"} priceMenu={"25000"}/>
+                    <FoodsMenu/>
+
+                    <h2>Drinks Menu</h2>
+                    <DrinksMenu/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/ComponentReact.js
+++ src/Components/ComponentReact.js
@@ -3,12 +3,10 @@ import React from "react";
 class FoodsMenu extends React.Component {
     render() {
         return(
-            <div className={"App"}>
-                <h1>Cafe and Resto</h1>
-                <h2>Foods Menu</h2>
+            <div>
                 <div style={{border: "1px solid black", width: 300, margin: 'auto'}}>
-                    <p>Menu Name: Chicken</p>
-                    <p>Price: Rp. 25.000</p>
+                    <p>Menu Name: {this.props.nameMenu}</p>
+                    <p>Price: {this.props.priceMenu}</p>
                 </div>
             </div>
         );
@@ -18,8 +16,7 @@ class FoodsMenu extends React.Component {
 class DrinksMenu extends React.Component {
     render() {
         return(
-            <div className={"App"}>
-                <h2>Drinks Menu</h2>
+            <div>
                 <div style={{border: "1px solid black", width: 300, margin: 'auto'}}>
                     <p>Menu Name: Melon Juice</p>
                     <p>Price: Rp. 15.000</p>
